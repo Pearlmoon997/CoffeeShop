f29eace1ab90af061c1850b07c487cda5d1b60c0
moongi <pearlmoon997@gmail.com>
2022-06-15 18:06:45 +0900

    Member CRUD

--- src/main/java/com/example/CoffeeShop/API/MemberAPIController.java
+++ src/main/java/com/example/CoffeeShop/API/MemberAPIController.java
@@ -7,10 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +30,24 @@ public class MemberAPIController {
         Member created = memberService.create(dto);
         return (created != null) ?
                 ResponseEntity.status(HttpStatus.OK).body(created) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    //회원 수정
+    @PatchMapping("/api/members/{id}")
+    public ResponseEntity<Member> update(@PathVariable Long id, @RequestBody MemberDto dto) {
+        Member updated = memberService.update(id, dto);
+        return (updated != null) ?
+                ResponseEntity.status(HttpStatus.OK).body(updated) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    //회원 삭제
+    @DeleteMapping("/api/members/{id}")
+    public ResponseEntity<Member> delete(@PathVariable Long id) {
+        Member deleted = memberService.delete(id);
+        return (deleted != null) ?
+                ResponseEntity.status(HttpStatus.OK).build() :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 }
--- src/main/java/com/example/CoffeeShop/Service/MemberService.java
+++ src/main/java/com/example/CoffeeShop/Service/MemberService.java
@@ -6,6 +6,7 @@ import com.example.CoffeeShop.Repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -22,11 +23,38 @@ public class MemberService {
     }
 
     //회원 등록
+    @Transactional
     public Member create(MemberDto dto) {
         Member member = dto.toEntity();
         if (member.getId() != null) {
             return null;
         }
         return memberRepository.save(member);
+    }
+
+    //회원 수정
+    @Transactional
+    public Member update(Long id, MemberDto dto) {
+        Member member = dto.toEntity();
+        log.info("id: {}, member: {}", id, member.toString());
+        Member target = memberRepository.findById(id).orElse(null);
+        if (target == null || id != member.getId()){
+            log.info("잘못된 요청");
+            return null;
+        }
+        target.patch(member);
+        Member updated = memberRepository.save(target);
+        return updated;
+    }
+
+    //회원 삭제
+    @Transactional
+    public Member delete(Long id) {
+        Member target = memberRepository.findById(id).orElse(null);
+        if (target == null) {
+            return null;
+        }
+        memberRepository.delete(target);
+        return target;
     }
 }
